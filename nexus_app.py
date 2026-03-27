0a54f4a15e93990a4fb3afeb2935efc207ecff29
ConsultorioQuevedo <luisraqueal@gmail.com>
2026-03-27 18:21:42 -0400

    Update nexus_app.py

--- nexus_app.py
+++ nexus_app.py
@@ -121,7 +121,7 @@ if opcion == "🏠 DASHBOARD":
     else:
         st.caption("No hay registros de tomas el día de hoy.")
 # --- 8. MÓDULO: FINANZAS (VERSIÓN LIMPIA) ---
-elif opcion == "💰 FINANZAS":
+elif opcion == "💰 FINANZASRD$":
     st.title("💰 Control de Finanzas - NEXUS PRO")
     st.markdown("---")
 
